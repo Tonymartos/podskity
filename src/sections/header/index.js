8e448b064b59b439a64001343ac35ac4224661bd
Tony Martos <tonymartos@protonmail.com>
2023-03-15 16:36:01 +0100

    LocalStorage save id and json from podcast

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -14,14 +14,27 @@ const RenderIcon = ({status}) => {
 }
 
 const Header = () => {
-    const statusStorageProcess = sessionStorage.getItem('processActive')
+    let statusStorageProcess = sessionStorage.getItem('processActive')
 
 
     const [sessionProcess, setSessionProcess] = useState(statusStorageProcess)
 
     useEffect(() => {
+
+        const expirationDuration = 1000 * 60 * 60 * 12; // 12 hours
+
+        const prevAccepted = localStorage.getItem("accepted");
+        const currentTime = new Date().getTime();
+
+        const notAccepted = prevAccepted == undefined;
+        const prevAcceptedExpired = prevAccepted != undefined && currentTime - prevAccepted > expirationDuration;
+        if (notAccepted || prevAcceptedExpired) {
+            localStorage.clear();
+            localStorage.setItem("accepted", currentTime);
+        }
+
         sessionStorage.setItem('processActive', 'active')
-    })
+    }, [])
 
     useEffect(() => {
         setSessionProcess(statusStorageProcess)
